063f1e95439e1302e75849ea581c661d274ab07c
lilacsheep <lilac123>
2018-05-13 23:04:17 +0800

    新增自动安装依赖库代码

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name='ucloud_sdk', version='0.1.13',
+setup(name='ucloud_sdk', version='0.1.14', install_requires=['requests', 'addict'],
       py_modules=[
           'ucloud_sdk.actions.base',
           'ucloud_sdk.actions.eip',
