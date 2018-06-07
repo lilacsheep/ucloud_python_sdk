0bc1086759b433078ad8bd3efcc43092c93bb37a
施杨 <shiyang@mu77.com>
2018-06-07 14:16:03 +0800

    新增返回BackendNotFound异常

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name='ucloud_sdk', version='0.1.22', install_requires=['requests', 'addict'],
+setup(name='ucloud_sdk', version='0.1.23', install_requires=['requests', 'addict'],
       py_modules=[
           'ucloud_sdk.actions.base',
           'ucloud_sdk.actions.eip',
@@ -11,9 +11,13 @@ setup(name='ucloud_sdk', version='0.1.22', install_requires=['requests', 'addict
           'ucloud_sdk.actions.uhost',
           'ucloud_sdk.actions.ulb',
           'ucloud_sdk.actions.umon',
+          'ucloud_sdk.actions.udb',
+          'ucloud_sdk.actions.uredis',
           'ucloud_sdk.models.uhost',
           'ucloud_sdk.models.eip',
           'ucloud_sdk.models.ulb',
+          'ucloud_sdk.models.udb',
+          'ucloud_sdk.models.uredis',
           'ucloud_sdk.client',
           'ucloud_sdk.exception',
           'ucloud_sdk.UCloud'],
